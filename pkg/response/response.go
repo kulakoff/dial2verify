ad009e43d20face1889f37bd5711ffc59b2d3d1d
Anton Kulakov <anton.kulakoff@ya.ru>
2025-09-13 10:32:35 +0300

    feat: added package response, some changes

---
+++ pkg/response/response.go
@@ -0,0 +1,28 @@
+package response
+
+type PhoneResponse struct {
+	Found  bool   `json:"found"`
+	Phone  string `json:"phone"`
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
+type PhoneResponseErr struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
+func SuccessPhoneCheck(phone string, found bool) *PhoneResponse {
+	return &PhoneResponse{
+		Found:  found,
+		Phone:  phone,
+		Status: "success",
+	}
+}
+
+func Error(msg string) *PhoneResponseErr {
+	return &PhoneResponseErr{
+		Status: "error",
+		Error:  msg,
+	}
+}
